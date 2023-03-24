81c2226ac0ba50f8d6bd262ebe17adc97adb95d3
Anuraag Agrawal <anuraaga@gmail.com>
2023-03-24 13:57:20 +0900

    Add test

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/anuraaga/binaryregexp-unicodefailure
+
+go 1.20
+
+require (
+	github.com/magefile/mage v1.14.0 // indirect
+	github.com/tetratelabs/wazero v1.0.0-rc.2 // indirect
+	github.com/wasilibs/go-re2 v0.1.1-0.20230324035920-bd770de810fa // indirect
+	rsc.io/binaryregexp v0.2.0 // indirect
+)
---
+++ regexp_test.go
@@ -0,0 +1,48 @@
+package repro
+
+import (
+	"fmt"
+	re2 "github.com/wasilibs/go-re2/experimental"
+	"rsc.io/binaryregexp"
+	"testing"
+)
+
+func TestUnicode(t *testing.T) {
+	tests := []struct {
+		pattern string
+		input   string
+		want    bool
+	}{
+		{
+			pattern: "ハロー",
+			input:   "ハローワールド",
+			want:    true,
+		},
+		{
+			pattern: "ハロー",
+			input:   "グッバイワールド",
+			want:    false,
+		},
+	}
+
+	for _, tc := range tests {
+		tt := tc
+		t.Run(fmt.Sprintf("%s/%s", tt.pattern, tt.input), func(t *testing.T) {
+			t.Run("re2", func(t *testing.T) {
+				re := re2.MustCompileLatin1(tt.pattern)
+				if re.MatchString(tt.input) != tt.want {
+					t.Errorf("MatchString(%q) = %v, want %v", tt.input, !tt.want, tt.want)
+				}
+			})
+		})
+
+		t.Run(fmt.Sprintf("%s/%s", tt.pattern, tt.input), func(t *testing.T) {
+			t.Run("binaryregexp", func(t *testing.T) {
+				re := binaryregexp.MustCompile(tt.pattern)
+				if re.MatchString(tt.input) != tt.want {
+					t.Errorf("MatchString(%q) = %v, want %v", tt.input, !tt.want, tt.want)
+				}
+			})
+		})
+	}
+}
